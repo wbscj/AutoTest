17945446f49f56e6c26f201cebe8ec7ea2fb2015
weifeng_wu <wmy_weifeng@163.com>
2022-01-26 21:53:31 +0800

    mac  运行

--- mybatisDemo/src/main/java/com/mybatis/model/Application.java
+++ mybatisDemo/src/main/java/com/mybatis/model/Application.java
@@ -3,10 +3,7 @@ package com.mybatis.model;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.annotation.PreDestroy;
