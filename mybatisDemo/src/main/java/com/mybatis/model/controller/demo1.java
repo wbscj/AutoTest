51a9de1ca7ed00dba758d9d9f53474c6b2e33dd1
weifeng_wu <wmy_weifeng@163.com>
2022-01-27 22:19:06 +0800

    添加user

--- mybatisDemo/src/main/java/com/mybatis/model/controller/demo1.java
+++ mybatisDemo/src/main/java/com/mybatis/model/controller/demo1.java
@@ -1,9 +1,11 @@
 package com.mybatis.model.controller;
 
+import com.mybatis.model.entity.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +23,18 @@ public class demo1 {
     public int getUserCount(){
         int count = template.selectOne("getUserCount");
         return count;
+    }
+
+    @RequestMapping(value = "/adduser" ,method = RequestMethod.POST)
+    @ApiOperation(value = "添加user 返回数量")
+    public int adduser(@RequestBody User user){
+        int num = template.insert("addUser",user);
+        return num;
+    }
+
+    @RequestMapping(value = "/adduser2" ,method = RequestMethod.POST)
+    public User adduser2(@RequestBody User user){
+        int num = template.insert("addUser",user);
+        return user;
     }
 }
